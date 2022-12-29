8153478e5b3f176eddc3da916c3501612d1e4a02
CarlosPimSilveira <carlospimsilveira@gmail.com>
2022-12-29 12:51:36 -0300

    testes

--- Exe-04/codigo.js
+++ Exe-04/codigo.js
@@ -1,8 +1,15 @@
 
 var nome = window.prompt('Qual é seu nome?');
+var salario = 10528.8
 
 document.write(`Seu nome tem <strong> ${nome.length} </strong> letras.`);
 
 document.write(`Seu nome ${nome.toUpperCase()}.`);
 
 document.write(`Seu nome ${nome.toLowerCase()}.`);
+
+salario.toFixed(2);
+
+salario.toFixed(2).replace('.', ',');
+
+salario.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
