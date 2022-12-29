4b8f2a5052fbe68e8803c2666cbe9218c446b6ee
CarlosPimSilveira <carlospimsilveira@gmail.com>
2022-12-29 00:20:52 -0300

    estudos

--- Exe-03/codigo.js
+++ Exe-03/codigo.js
@@ -7,5 +7,14 @@ var s = n1 + n2;
 
 window.alert('Soma dos valores é: '+ s);
 
+
+var nome = window.prompt('Digite seu nome: ');
+
+var idade = window.prompt('Digite sua idade: ');
+
+var nota = window.prompt('Digite sua nota: ');
+
+window.alert(`O aluno ${nome} com ${idade} anos tirou a nota ${nota}`);
+
 // (number + number)  para adição
 // (string + string)  para concatenação
---
+++ Exe-04/codigo.js
@@ -0,0 +1,8 @@
+
+var nome = window.prompt('Qual é seu nome?');
+
+document.write(`Seu nome tem <strong> ${nome.length} </strong> letras.`);
+
+document.write(`Seu nome ${nome.toUpperCase()}.`);
+
+document.write(`Seu nome ${nome.toLowerCase()}.`);
