335721611b2e6fea1954e8068e9a72615172aebe
CarlosPimSilveira <carlospimsilveira@gmail.com>
2022-12-28 23:44:14 -0300

    Novos Ex

---
+++ Exe-03/codigo.js
@@ -0,0 +1,11 @@
+
+var n1 = Number.parseInt(window.prompt('Digite um numero: ')); //inteiro convertido
+
+var n2 = Number.parseInt(window.prompt('Digite um segundo numero: ')); //inteiro convertido
+
+var s = n1 + n2;
+
+window.alert('Soma dos valores é: '+ s);
+
+// (number + number)  para adição
+// (string + string)  para concatenação
