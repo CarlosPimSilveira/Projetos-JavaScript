7a2f3def29a6e8c0f03b18284c471a509a2f1392
CarlosPimSilveira <carlospimsilveira@gmail.com>
2023-01-08 22:09:47 -0300

    Condição

---
+++ Exe-08/index.js
@@ -0,0 +1,9 @@
+var vel = 60.5
+
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+
+if(vel > 60){ // condiçãoa simples!
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+
+console.log(`Digija sempre usando cinto de segurança!`)
